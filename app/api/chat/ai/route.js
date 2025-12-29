bcc2687e765ddf36acedab08f40a8638bddc49d2
AdinathBorse <adinathborse02@gmail.com>
2025-12-29 21:10:02 +0530

    change-in sk

--- app/api/chat/ai/route.js
+++ app/api/chat/ai/route.js
@@ -8,7 +8,7 @@ import Chat from "@/models/Chat";
 
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
-  apiKey: process.env.OPENROUTER_API_KEY,
+  apiKey: process.env.DEEPSEEK_API_KEY,
   defaultHeaders: {
     "HTTP-Referer": "http://localhost:3000",
     "X-Title": "My NextJS App",
